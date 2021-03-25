644da48aa2f51c842cdee52fd864b71e7379c253
malcolm <Malcom@Malcolms-MacBook-Pro.local>
2021-03-25 16:59:00 +0000

    created my first file in node js, will update the readme with the resource I'm using

--- myfirstjs.js
+++ myfirstjs.js
@@ -1,8 +1,18 @@
 var http = require("http")
+var fs = require('fs')
+
 var dateVar = require('./myFirstNodeModule')
+
+
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.write('You typed in the url : '+req.url+'<br/>');
-    res.write('Hellow Node Js, my first module is a date one that givves back: ' + dateVar.myDateTime())
-    res.end()
+    fs.readFile('demo.html',function (err, data){
+        res.writeHead(200, {'Content-type': 'text/html'})
+        fs.appendFile('myFirstNodeJsCreatedFile.txt', req.url, function (error){
+            if (error) throw error
+            console.log("saved")
+        });
+        res.write('Hellow Node Js, my first module is a date one that givves back: ' + dateVar.myDateTime())
+        res.write(data)
+        res.end()
+    })
 }).listen(8081)
