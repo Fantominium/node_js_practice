97b15cace7a410fe43113aacc7042e29df9aaaee
malcolm <root@Malcolms-MacBook-Pro.local>
2021-03-27 11:09:07 +0000

    fundamentals down, will attempt basic connection between node and react now

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Following the tutorial on https://www.w3schools.com/nodejs/nodejs_intro.asp
 Currently on Node Url module.
 
 
+Update: - Some of the packages to be installed via npm in the tutorial requires 
+access to some laptop facilities like images and calendar, mail etc. so permissions need to be adjusted to accomodate
+
 
 To think about:
 Could I use node functions in react to store state in say a file? 
--- myfirstjs.js
+++ myfirstjs.js
@@ -1,17 +1,32 @@
 var http = require("http")
 var fs = require('fs')
-
+var url = require('url')
 var dateVar = require('./myFirstNodeModule')
+var events = require('events')
+var eventEmitter = new events.EventEmitter()
+
+var myEventHandler = () => {
+    console.log('Scream if you wish!')
+}
+eventEmitter.on('scream', myEventHandler)
+eventEmitter.emit('scream')
 
 
 http.createServer(function(req, res){
-    fs.readFile('demo.html',function (err, data){
+    var q = url.parse(req.url, true)
+    var filename = '.' + q.pathname
+
+    fs.readFile(filename,function (err, data){
+        if (err) {
+            res.writeHead(400, {'Content-type': 'text/html'})
+            return res.end('404 not found for ' + req.url)
+        }
         res.writeHead(200, {'Content-type': 'text/html'})
         fs.appendFile('myFirstNodeJsCreatedFile.txt', req.url, function (error){
             if (error) throw error
             console.log("saved")
         });
-        res.write('Hellow Node Js, my first module is a date one that givves back: ' + dateVar.myDateTime())
+        res.write('Hellow Node Js, my first module is a date one that gives back: ' + dateVar.myDateTime())
         res.write(data)
         res.end()
     })
